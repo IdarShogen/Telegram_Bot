b1b3373a0aa403e074f5edaf28a301c8e4687de2
IdarShogen <idarsogenov0@gmail.com>
2025-12-11 18:21:18 +0300

    Test commit nout -> git -> pc

--- src/test/java/BotTest.java
+++ src/test/java/BotTest.java
@@ -2,8 +2,7 @@
 
 public class BotTest {
     public static void main(String[] args) {
-        System.out.println("АААААААА");
-        System.out.println("СЮЮЮЮ");
+        System.out.println("А ТЕПЕРЬ ПРОВЕРИМ С НОУТА НА ГИТ И С ГИТА НА ПК!");
 
     }
 }
