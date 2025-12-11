ca24f6481ba94e03cbc91abe9305f84f7f4b15b6
IdarShogen <idarsogenov0@gmail.com>
2025-12-11 17:15:53 +0300

    New commit

--- src/main/java/org/example/UserStateService.java
+++ src/main/java/org/example/UserStateService.java
@@ -6,5 +6,14 @@ import java.util.concurrent.ConcurrentHashMap;
 public class UserStateService {
     private static final Map<Long, UserState> state = new ConcurrentHashMap<Long, UserState>();
 
+    public static void setState(long chatId, UserState userState) {
+        state.put(chatId, userState);
+    }
+    public static UserState getState(long chatId) {
+        return state.getOrDefault(chatId, UserState.IDLE);
+    }
 
+    public static void clearState(long chatId) {
+        state.remove(chatId);
+    }
 }
